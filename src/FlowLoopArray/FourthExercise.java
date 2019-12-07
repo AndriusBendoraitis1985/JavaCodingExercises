5b97477eebe25527c9caa24fd335c84d3c42abeb
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-12-07 14:32:24 +0200

    Write an application that for any entered number between 0 and 9 will provide it's name

---
+++ src/FlowLoopArray/FourthExercise.java
@@ -0,0 +1,52 @@
+package FlowLoopArray;
+
+import java.util.Scanner;
+
+public class FourthExercise {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Iveskite skaiciu");
+        int number = input.nextInt();
+
+        switch (number){
+            case 1:{
+                System.out.println("one");
+                break;
+            }
+            case 2:{
+                System.out.print("two");
+                break;
+            }
+            case 3:{
+                System.out.print("three");
+                break;
+            }
+            case 4:{
+                System.out.print("four");
+                break;
+            }
+            case 5:{
+                System.out.print("five");
+                break;
+            }
+            case 6:{
+                System.out.print("six");
+                break;
+            }
+            case 7:{
+                System.out.print("seven");
+                break;
+            }
+            case 8:{
+                System.out.print("eigth");
+                break;
+            }
+            case 9:{
+                System.out.print("nine");
+                break;
+            }
+        }
+
+    }
+}
