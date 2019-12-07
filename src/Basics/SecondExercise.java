085225f69f30c970985138af62ea857d140aeecd
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-12-07 12:04:43 +0200

    Display the given value rounded to two decimal places.

--- src/Basics/SecondExercise.java
+++ src/Basics/SecondExercise.java
@@ -3,6 +3,9 @@ package Basics;
 public class SecondExercise {
 
     public static void main (String []args ){
+        double number = 23.4567;
+
+        System.out.printf("%.2f",number);
 
     }
 }
