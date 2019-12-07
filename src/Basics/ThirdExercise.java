194bf0c7217239a805f422bff384bd3c9df41e1f
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-12-07 12:10:55 +0200

    Display their division casted to the double type and rounded to the third decimal point.

---
+++ src/Basics/ThirdExercise.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class ThirdExercise {
+
+    public static void main(String[] args) {
+        int numberOne = 20;
+        int numberTwo = 3;
+
+        double answer = (double) numberOne / numberTwo;
+
+        System.out.println(answer);
+        System.out.printf("%.3f", answer);
+
+    }
+}
