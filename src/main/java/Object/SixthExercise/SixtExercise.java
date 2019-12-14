1e04884a9f168e0f2e92f92713ae435795b21dc5
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-12-14 16:02:47 +0200

    Ilgiausio palindromo radimas: almost done;

--- src/main/java/Object/SixthExercise/SixtExercise.java
+++ src/main/java/Object/SixthExercise/SixtExercise.java
@@ -1,32 +1,69 @@
 package Object.SixthExercise;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SixtExercise {
+
+    private static final String READ_FROM_FILE = "C:\\Users\\Vartotojas\\IdeaProjects\\JavaCodingExercises\\src\\main\\java\\Object\\SixthExercise\\input.txt";
+    private static final String WRITE_TO_FILE = "C:\\Users\\Vartotojas\\IdeaProjects\\JavaCodingExercises\\src\\main\\java\\Object\\SixthExercise\\output.txt";
+
     public static void main(String[] args) {
-       //String line = "ABAGTGABA";
-        String line = "ABABA";
-        //String line = "LABAS";
-        char[] input = line.toCharArray();
-        StringBuilder stringBuilder = new StringBuilder();
-        int max = 0;
-        String maxWord="";
 
+        List<Input> inputList = getCharFromFile();
 
-        for (int i = 0; i < input.length; i++) {
-            stringBuilder.append(input[i]);
-            String reverseText = stringBuilder.reverse().toString();
-            stringBuilder.reverse();
-            System.out.println(stringBuilder);
-            System.out.println(reverseText);
+        for (Input input: inputList){
+            getLongestPalindrome(input.getInput());
+            System.out.println(getLongestPalindrome(input.getInput()));
+        }
+    }
 
-            if (stringBuilder.toString().equals(reverseText)) {
-                System.out.println("ok");
-                if (max<i){
-                    max = i;
-                    maxWord=stringBuilder.toString();
+    private static String getLongestPalindrome(char[] input){
+        String maxWord = "";
+        int maxWordLenth=0;
+
+        for (int j = 0; j < input.length-1; j++) {
+            StringBuilder stringBuilder = new StringBuilder(input[j]);
+            for (int i = j; i < input.length; i++) {
+                stringBuilder.append(input[i]);
+                String reverseText = stringBuilder.reverse().toString();
+                stringBuilder.reverse();
+
+                if (stringBuilder.toString().equals(reverseText)) {
+                    if (maxWordLenth<stringBuilder.toString().length()) {
+                        maxWordLenth=stringBuilder.toString().length();
+                        maxWord = stringBuilder.toString();
+                    }
                 }
             }
-        } System.out.println(max);
-        System.out.println(maxWord);
+        }
+        return maxWord;
+    }
+
+    private static List<Input> getCharFromFile (){
+        List<Input> inputList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(READ_FROM_FILE))){
+            String line = "";
+
+            while (line != null){
+                line = br.readLine();
+                if (line == null){
+                    break;
+                }
+                char [] inputWord = line.toCharArray();
+                inputList.add(mapWordDataToObject(inputWord));
+            }
+        } catch (IOException e){
+            System.out.println("Klaida nuskaitant faila");
+        }
+        return inputList;
+    }
+    private static Input mapWordDataToObject(char[] wordData){
+        return new Input(wordData);
     }
 }
 
